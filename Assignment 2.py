8b93eca29114078ca4a21d410a5ad5cadb2e63c6
Shahzaib19 <38549171+Shahzaib19@users.noreply.github.com>
2019-11-15 03:24:54 +0500

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 01:42:32 2019
+
+@author: moham
+"""
+#QNO 1
+'''print("QNO:1")
+print("\n")
+totalMarks = 500
+urdu = int(input("Enter The Urdu Marks: "))
+english = int(input("Enter the English Marks: "))
+chemistry = int(input("Enter the Chemistry Marks: "))
+physics = int(input("Enter the Physics Marks: "))
+math = int(input("Enter the Math Marks: "))
+obtMarks = urdu + english + chemistry + physics + math
+percnt = obtMarks / totalMarks * 100
+print("\n")
+print("Your Percentage is: "+ str(percnt))
+if percnt >= 80 and percnt < 100:
+   print("your Grade is "+"A+")
+elif percnt >= 70 and percnt < 80:
+   print('Your Grade is '+ "A")
+elif percnt >= 60 and percnt < 70:
+   print("Your Grade is "+ "B")
+elif percnt >= 50 and percnt <60:
+    print("Your Grade is "+ "C")
+else:
+    print("you Failed")
+'''
+'''
+#QNO:2
+print("QNO2")
+print("\n")
+number = int(input("Enter the Number : "))
+if (number%2==0):
+    print("your Number is even :" + str(number))
+else:
+    print("odd Number :" + str(number))
+'''
+#Qno3
+print("QNO3")
+print("\n")
+
+list = 82,53,81,50
+length = len(list)
+print("the list length is : " + str(length))
+
+#QNO4
+print('QNO4')
+print("\n")
+s=sum(list)
+print("the sum of list numeric is : " + str(s))
+
+#QNO5
+print('QNO5')
+print("\n")
